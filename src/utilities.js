f7608705823dcb8d2ba5a46ec2dac80117514a69
Meet <meetdomadia789@gmail.com>
2025-05-08 00:14:45 +0530

    Initial commit

--- src/utilities.js
+++ src/utilities.js
@@ -6,14 +6,17 @@ export const drawRect = (detections, ctx) =>{
     const [x, y, width, height] = prediction['bbox']; 
     const text = prediction['class']; 
 
+    console.log(text);
+    
+
     // Set styling
-    const color = Math.floor(Math.random()*16777215).toString(16);
-    ctx.strokeStyle = '#' + color
-    ctx.font = '18px Arial';
+    // const color = Math.floor(Math.random()*16777215).toString(16);
+    ctx.strokeStyle = '#ffffff'
+    ctx.font = '23px Arial';
 
     // Draw rectangles and text
     ctx.beginPath();   
-    ctx.fillStyle = '#' + color
+    ctx.fillStyle = '#ffffff'
     ctx.fillText(text, x, y);
     ctx.rect(x, y, width, height); 
     ctx.stroke();
